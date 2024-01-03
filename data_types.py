a593fa2daa0c11b8f8c8df4726c97ad8114fb9c7
Alisa Orlova <alor@mir-robots.com>
2024-01-03 14:18:40 +0100

    Add data types

--- data_types.py
+++ data_types.py
@@ -24,3 +24,38 @@ class SweepSeries:
 
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__)
+
+
+@dataclass
+class raw_sweep:
+    sec: int
+    nsec: int
+    ranges: list
+
+
+@dataclass
+class EuclidianCoordinate:
+    x: float
+    y: float
+
+
+@dataclass
+class Blob:
+    points: list[EuclidianCoordinate]
+    center_of_mass: EuclidianCoordinate
+    velocity_vector: tuple
+
+
+@dataclass
+class Sweep:
+    sweep_nr: int
+    all_points: list[EuclidianCoordinate]
+    blobs: list[Blob]
+
+
+@dataclass
+class SweepSeries:
+    sweeps: list[Sweep]
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
